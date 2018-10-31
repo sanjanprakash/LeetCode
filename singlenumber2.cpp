27cab0c13e9de1349b78ed84343b88989b70b9d5
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2018-10-31 13:27:20 -0400

    Single Number 2

---
+++ singlenumber2.cpp
@@ -0,0 +1,8 @@
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        unique = set(nums)
+        return (3*sum(unique) - sum(nums))/2
