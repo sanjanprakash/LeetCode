0c6939239334b55cdbf31852118ec46d8b2fdba4
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2018-11-23 21:05:38 -0500

    Find Bottom Left Tree Value

---
+++ bottomleft.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int findBottomLeftValue(TreeNode* root) {
+        vector<vector<int> > ans;
+        queue<TreeNode *> Q;
+        if (root != NULL) {
+            vector<int> level;
+            TreeNode *curr;
+            int s = 1;
+            Q.push(root);
+            while (!Q.empty()) {
+                curr = Q.front();
+                Q.pop();
+                level.push_back(curr->val);
+                s--;
+                if (curr->left != NULL)
+                    Q.push(curr->left);
+                if (curr->right != NULL)
+                    Q.push(curr->right);
+                if (s == 0) {
+                    s = Q.size();
+                    ans.push_back(level);
+                    level.clear();
+                }
+            }
+        }
+        return ans[ans.size()-1][0];    
+    }
+};
