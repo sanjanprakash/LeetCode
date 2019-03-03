d0b247ed2ca746f83a047d21aea7c6b59d7f5151
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2019-03-03 17:03:32 -0500

    Fibonacci Memoization

---
+++ fibonacci_memoization.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int fib(int N) {
+        if (N < 2)
+            return N;
+        int i;
+        int *arr = new int[N+1];
+        for (i = 0; i < N+1; i++) {
+            if (i < 2)
+                arr[i] = i;
+            else
+                arr[i] = -1;
+        }
+        return helper(arr,N);
+    }
+    
+    int helper(int *arr, int N) {
+        if (arr[N] != -1)
+            return arr[N];
+        arr[N] = helper(arr,N-1) + helper(arr,N-2);
+        return arr[N];
+    }
+};
