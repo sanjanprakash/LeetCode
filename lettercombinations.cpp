2008c6462eb99cda2f4449695ab8e13518620a86
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2019-03-10 01:02:33 -0500

    Letter Combinations of a Phone Number

--- lettercombinations.cpp
+++ lettercombinations.cpp
@@ -1,40 +1,32 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
+        int numLetters[10] = {0,0,3,3,3,3,3,4,3,4};
+        char letters[10] = {'_','_','a','d','g','j','m','p','t','w'};
+        int i,j,k;
+        int num;
+        int prev = 0;
         vector<string> ans;
-        vector<char> X;
-        vector<vector <char> > dict;
-        dict.push_back(X);
-        dict.push_back(X);
-        int i,j,k,letters = 97,num,num_prev;
-        for (i = 0; i < 8; i++) {
-            vector<char> X;
-            j = 3;
-            if (i == 5 || i == 7)
-                j = 4;
-            for (k = 0; k < j; k++)
-                X.push_back(char(letters+k));
-            letters += k;
-            dict.push_back(X);
-        }
-        for (i = 0; i < digits.length(); i++) {
-            string temp;
-            num = int(digits[i]) - 48;
-            if (i == 0) {
-                for (j = 0; j < dict[num].size(); j++) {
-                    temp = dict[num][j];
+        string temp;
+        for (k = 0; k < digits.length(); k++) {
+            num = digits[k] - '0';
+            if (k == 0) {
+                for (i = 0; i < numLetters[num]; i++) {
+                    temp = char(letters[num]+i);
                     ans.push_back(temp);
                 }
             }
             else {
-                num_prev = ans.size();
-                for (j = 1; j < dict[num].size(); j++) {
-                    for (k = 0; k < num_prev; k++)
-                        ans.push_back(ans[k] + dict[num][j]);
+                for (j = 0; j < numLetters[num]-1; j++) {
+                    for (i = 0; i < prev; i++)
+                        ans.push_back(ans[i]);
                 }
-                for (j = 0; j < num_prev; j++)
-                    ans[j] = ans[j] + dict[num][0];
+                for (j = 0; j < numLetters[num]; j++) {
+                    for (i = j*prev; i < (j+1)*prev; i++)
+                        ans[i] += char(letters[num]+j);
+                }
             }
+            prev = ans.size();
         }
         return ans;
     }
