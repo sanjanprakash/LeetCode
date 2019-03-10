f93883bc3afecf10c936ef1b3a7047fc8fe05607
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2019-03-10 01:38:28 -0500

    Maximum Subarray

--- maximumsubarray.cpp
+++ maximumsubarray.cpp
@@ -1,37 +1,14 @@
 class Solution {
 public:
-    int crossSubarray(vector<int> nums, int l, int m, int r) {
-        int i;
-        int lsum = 0, rsum = 0;
-        int lmax = INT_MIN, rmax = INT_MIN;
-        i = m;
-        while (i >= l) {
-            lsum += nums[i];
-            lmax = max(lmax,lsum);
-            i--;
-        }
-        i = m+1;
-        while (i <= r) {
-            rsum += nums[i];
-            rmax = max(rmax,rsum);
-            i++;
-        }
-        return lmax+rmax;
-    }
-    
-    int maxSumSubarray(vector<int> nums, int l, int r) {
-        if (l < r) {
-            int m = l + (r-l)/2;
-            int left = maxSumSubarray(nums,l,m);
-            int right = maxSumSubarray(nums,m+1,r);
-            int cross = crossSubarray(nums,l,m,r);
-            int ans = max(cross,max(left,right));
-            return ans;
-        }
-        return nums[l];
-    }
-    
     int maxSubArray(vector<int>& nums) {
-        return maxSumSubarray(nums,0,nums.size()-1);    
+        int ans = nums[0];
+        int i, sum = nums[0];
+        for (i = 1; i < nums.size(); i++) {
+            sum += nums[i];
+            if (nums[i] > sum)
+                sum = nums[i];
+            ans = max(sum,ans);
+        }
+        return ans;
     }
 };
