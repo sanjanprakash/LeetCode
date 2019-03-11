b852ace8e1d469a7f510c16b7e208c94a7250ddd
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2019-03-11 00:46:38 -0400

    Kth Largest Element in an Array

--- kthlargestelem.cpp
+++ kthlargestelem.cpp
@@ -1,7 +1,36 @@
 class Solution {
 public:
+    int partition(vector<int> &nums, int l, int r) {
+        int i,j = l;
+        int size = r-l;
+        int pivot_idx = rand() % (size+1);
+        swap(nums[r],nums[l+pivot_idx]);
+        int pivot = nums[r];
+        for (i = l; i < r; i++) {
+            if (nums[i] <= pivot) {
+                swap(nums[i],nums[j]);
+                j++;
+            }
+        }
+        swap(nums[j],nums[r]);
+        return j;
+    }
+    
+    int kthLargestHelper(vector<int> &nums, int l, int r, int k) {
+        if (0 < k && k <= r-l+1) {
+            int pivot = partition(nums,l,r);
+            if (pivot-l == k-1)
+                return nums[pivot];
+            if (pivot-l > k-1)
+                return kthLargestHelper(nums,l,pivot-1,k);
+            else
+                return kthLargestHelper(nums,pivot+1,r,k-pivot+l-1);
+        }
+        return INT_MAX;
+    }
+    
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(),nums.end());
-        return nums[nums.size() - k];
+        int asc_order = nums.size() - k + 1;
+        return kthLargestHelper(nums,0,nums.size()-1,asc_order);
     }
 };
