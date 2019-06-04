b72a8f173abc0dbfefec289aecaf2cf1e2b7975e
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2019-06-04 14:48:57 -0400

    Binary Tree Path

---
+++ binarytreepath.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:    
+    void pathFinder(TreeNode *ptr, vector<string> &paths, string curr) {
+        if (ptr) {
+            curr = curr + "->" + to_string(ptr->val);
+            pathFinder(ptr->left,paths,curr);
+            pathFinder(ptr->right,paths,curr);
+            if (!ptr->left && !ptr->right)
+                paths.push_back(curr);
+        }
+    }
+    
+    vector<string> binaryTreePaths(TreeNode* root) {
+        string curr;
+        vector<string> paths;
+        if (root) {
+            curr = to_string(root->val);
+            pathFinder(root->left,paths,curr);
+            pathFinder(root->right,paths,curr);
+        }
+        if (!paths.size() && root)
+            paths.push_back(curr);
+        return paths;
+    }
+};
