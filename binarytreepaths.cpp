bd53a9e3a88f91b98f380555cf5dd8abecee8a2e
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2019-06-04 15:42:03 -0400

    Binary Tree Paths

--- binarytreepaths.cpp
+++ binarytreepaths.cpp
@@ -1,3 +1,12 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
 public:    
     void pathFinder(TreeNode *ptr, vector<string> &paths, string curr) {
