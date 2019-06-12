afd170780ae9a63f9dfd9669a1a0524e272d44b6
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2019-06-12 15:20:37 -0400

    Cousins in Binary Tree

--- cousinsbinarytree.cpp
+++ cousinsbinarytree.cpp
@@ -9,15 +9,15 @@
  */
 class Solution {
 public:
-    int finder(TreeNode *root, int &par, int h, int val) {
+    int finder(TreeNode *root, TreeNode* &par, int h, int val) {
         int l = 0,r = 0;
         if (root) {
             if (root->val == val) 
                 return h;
-            par = root->val;
+            par = root;
             l = finder(root->left,par,h+1,val);
             if (!l) 
-                par = root->val;
+                par = root;
             return l ? l : finder(root->right,par,h+1,val);
         }
         return 0;
@@ -25,7 +25,7 @@ public:
     
     bool isCousins(TreeNode* root, int x, int y) {
         int h_x, h_y;
-        int par_x = 0, par_y = 0;
+        TreeNode *par_x = NULL, *par_y = NULL;
         h_x = finder(root,par_x,0,x);
         h_y = finder(root,par_y,0,y);
         return h_x && h_y && h_x == h_y && par_x != par_y;
