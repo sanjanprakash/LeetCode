f0957945bd3b6093cf7909e48080809914dee236
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2019-06-27 01:28:27 -0400

    House Robber

--- houserobber.cpp
+++ houserobber.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0)
+        int i, n = nums.size();
+        vector<int> dp(n+1,0);
+        if (!n)
             return 0;
-        if (n == 1)
-            return nums[0];
-        if (n == 2)
-            return max(nums[0],nums[1]);
-        int i;
-        int *money = new int[n];
-        money[0] = nums[0];
-        money[1] = max(nums[1],money[0]);
-        for (i = 2; i < n; i++) {
-            money[i] = max(money[i-2]+nums[i],money[i-1]);
+        else {
+            dp[1] = nums[0];
+            for (i = 2; i < n+1; i++)
+                dp[i] = max(dp[i-1],dp[i-2]+nums[i-1]);
+            return dp[n];
         }
-        return money[n-1];
     }
 };
