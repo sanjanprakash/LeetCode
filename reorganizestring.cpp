14369d006850311bd8b9e58701ea25da3a8f000d
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2019-07-03 01:03:47 -0400

    Reorganize String

---
+++ reorganizestring.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    static bool compare(pair<int,char> A, pair<int,char> B) {
+        return A.first > B.first;
+    }
+    
+    string reorganizeString(string S) {
+        int i, j = 0, n = S.length();
+        vector<pair<int,char> > freqs;
+        string ans = S;
+        for (i = 0; i < 26; i++)
+            freqs.push_back(make_pair(0,97+i));
+        for (i = 0; i < n; i++) {
+            freqs[S[i]-'a'].first++;
+            if (n%2 && freqs[S[i]-'a'].first > n/2 + 1)
+                return "";
+            else if (n%2 == 0 && freqs[S[i]-'a'].first > n/2)
+                return "";
+        }
+        sort(freqs.begin(),freqs.end(),compare);
+        i = 0;
+        while (i < n) {
+            ans[i] = freqs[j].second;
+            freqs[j].first--;
+            i += 2;
+            if (!freqs[j].first)
+                j++;
+        }
+        i = 1;
+        while (i < n) {
+            ans[i] = freqs[j].second;
+            freqs[j].first--;
+            i += 2;
+            if (!freqs[j].first)
+                j++;
+        }
+        return ans;
+    }
+};
