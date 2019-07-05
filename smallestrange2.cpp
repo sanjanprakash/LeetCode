c7ee415673ede39a28d7fb5e5e8df56d510ad9f8
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2019-07-04 20:27:38 -0400

    Smallest Range II

--- smallestrange2.cpp
+++ smallestrange2.cpp
@@ -6,7 +6,7 @@ public:
         if (n < 2)
             return 0;
         sort(A.begin(),A.end());
-        ans = A[n-1] - A[0];
+        ans = A[n-1]-A[0];
         for (i = 0; i < n-1; i++)
             ans = min(ans,max(A[n-1]-K,A[i]+K) - min(A[0]+K,A[i+1]-K));
         return ans;        
