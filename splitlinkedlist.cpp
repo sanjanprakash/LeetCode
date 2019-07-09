a733f27ec79b65b6f42654e9dbc0ca51d7eb58dc
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2019-07-09 12:42:49 -0400

    Split Linked List in Parts

--- splitlinkedlist.cpp
+++ splitlinkedlist.cpp
@@ -11,32 +11,28 @@ public:
     vector<ListNode*> splitListToParts(ListNode* root, int k) {
         int i, len = 0;
         int size, rem;
-        vector<int> sizes(k,0);
         vector<ListNode *> ans;
-        ListNode *curr = root, *start, *prev;
+        ListNode *curr = root, *prev;
         while (curr) {
             curr = curr->next;
             len++;
         }
         size = len/k;
         rem = len%k;
-        for (i = 0; i < k; i++)
-            sizes[i] += size;
-        for (i = 0; i < k && rem; i++) {
-            sizes[i]++;
-            rem--;
-        }
         curr = root;
         for (i = 0; i < k; i++) {
-            size = 0;
-            start = curr;
-            while (size < sizes[i]) {
+            len = 0;
+            ans.push_back(curr);
+            while (len < size) {
                 prev = curr;
                 curr = curr->next;
-                size++;
+                len++;
+            }
+            if (ans.size() <= rem) {
+                prev = curr;
+                curr = curr->next;
             }
             prev->next = NULL;
-            ans.push_back(start);
         }
         return ans;
     }
