0266c2ab85911a524b65400f5a4da956908b8fe8
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2019-07-21 02:34:28 -0400

    Single Number III

--- singlenumber3.cpp
+++ singlenumber3.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
         int all_xor = 0;
-        int div = 1;
-        int isolated_set_bit, set_a = 0 , set_b = 0;
+        int isolated_set_bit;
         vector<int>ans(2,0);
         for (auto &x : nums)
             all_xor ^= x;
         isolated_set_bit = all_xor & ~(all_xor-1);
         for (auto &x : nums) {
             if (x & isolated_set_bit)
-                set_a ^= x;
+                ans[0] ^= x;
             else
-                set_b ^= x;
+                ans[1] ^= x;
         }
-        ans[0] = set_a;
-        ans[1] = set_b;
         return ans;
     }
 };
