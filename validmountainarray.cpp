a22526c488c60ef5a96246e549114e420032d0eb
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2019-07-22 19:53:00 -0400

    Valid Mountain Array

---
+++ validmountainarray.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& A) {
+        int i = 0, n = A.size();
+        if (n < 3)
+            return false;
+        while (i+1 < n && A[i] < A[i+1])
+            i++;
+        if (i == 0 || i == n-1)
+            return false;
+        while (i+1 < n && A[i] > A[i+1])
+            i++;
+        return i == n-1;
+    }
+};
