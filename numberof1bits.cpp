d27e6c4560d783f85f1b20e6502b9b8e6e173f87
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2019-07-21 21:52:02 -0400

    Number of 1 Bits

---
+++ numberof1bits.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int ans = 0;
+        while (n) {
+            if (n%2)
+                ans++;
+            n /= 2;
+        }
+        return ans;
+    }
+};
