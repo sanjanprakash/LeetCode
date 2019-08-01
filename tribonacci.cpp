9cb92a7d1cb08a63bcfc58d0ea489c7d6d90a354
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2019-08-01 18:13:25 -0400

    N-th Tribonacci Number

--- tribonacci.cpp
+++ tribonacci.cpp
@@ -3,14 +3,12 @@ public:
     int tail_trib(int a, int b, int c, int n) {
         if (n == 2)
             return c;
-        if (n == 1)
-            return b;
-        if (n == 0)
-            return a;
         return tail_trib(b,c,a+b+c,n-1);
     }
     
     int tribonacci(int n) {
+        if (n < 2)
+            return n;
         return tail_trib(0,1,1,n);
     }
 };
