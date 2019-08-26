f0df42a4e5560ee45cd7491ff56471c978fbec70
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2019-08-25 23:07:32 -0400

    Longest Increasing Subsequence

--- lis.cpp
+++ lis.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 0)
-            return 0;
-        int arr[n];
-        arr[0] = 1;
-        int i, j;
-        int count = 1, ans;
-        int lis_i;
-        for (i = 1; i < n; i++) {
-            lis_i = INT_MIN;
-            for (j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) 
-                    lis_i = max(lis_i,arr[j]);
-            }
-            if (lis_i == INT_MIN)
-                arr[i] = 1;
+    int upper_bound(vector<int>& nums, int t) {
+        int l = 0, r = nums.size(), m;
+        while (l < r) {
+            m = l + (r-l)/2;
+            if (nums[m] == t)
+                return m;
+            if (nums[m] < t)
+                l = m+1;
             else
-                arr[i] = 1 + lis_i;
+                r = m;
         }
-        for (i = 0; i < n; i++)
-            cout << arr[i] << " ";
-        cout << endl;
-        ans = arr[0];
-        for (i = 1; i < n; i++) {
-            if (ans < arr[i])
-                ans = arr[i];
+        if (nums[l] < t)
+            return l+1;
+        return l;
+    }
+    
+    int lengthOfLIS(vector<int>& nums) {
+        int i, n = nums.size();
+        vector<int> dp;
+        if (n) {
+            dp.push_back(nums[0]);
+            for (i = 1; i < n; i++) {
+                if (nums[i] < dp[0])
+                    dp[0] = nums[i];
+                else if (nums[i] > dp[dp.size()-1])
+                    dp.push_back(nums[i]);
+                else
+                    dp[upper_bound(dp,nums[i])] = nums[i];
+            }
         }
-        return ans;
+        return dp.size();
     }
 };
