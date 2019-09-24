40a28ad7fab343d458dc6312d9652fd97bac022e
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2019-09-23 20:01:48 -0400

    N-Queens

---
+++ nqueens.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<bool> visited_columns(n,false);
+        vector<string> grid(n,string(n,'.'));
+        vector<vector<string> > ans;
+        dfs(grid,visited_columns,ans,n);
+        return ans;
+    }
+    
+    void dfs(vector<string>& grid, vector<bool>& visited, vector<vector<string> >& ans, int n) {
+        int i, dim = grid[0].length();
+        if (!n) {
+            ans.push_back(grid);
+            return;
+        }
+        for (i = 0; i < dim; i++) {
+            if (!visited[i] && queen_possible(grid,n-1,i)) {
+                visited[i] = true;
+                grid[n-1][i] = 'Q';
+                dfs(grid,visited,ans,n-1);
+                grid[n-1][i] = '.';
+                visited[i] = false;
+            }
+        }        
+    }
+    
+    bool queen_possible(vector<string>& grid, int row, int col) {
+        int n = grid[0].length();
+        int l = col, r = col;
+        while (row < n) {
+            if (l >= 0 && grid[row][l] == 'Q')
+                return false;
+            if (r < n && grid[row][r] == 'Q')
+                return false;
+            if (grid[row][col] == 'Q')
+                return false;
+            row++;
+            l--;
+            r++;
+        }
+        return true;        
+    }
+};
