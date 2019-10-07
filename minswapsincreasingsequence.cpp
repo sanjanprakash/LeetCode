95914288439b10e8569ab99f25198257b7da1f0e
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2019-10-06 21:32:18 -0400

    Minimum Swaps To Make Sequences Increasing

--- minswapsincreasingsequence.cpp
+++ minswapsincreasingsequence.cpp
@@ -6,15 +6,17 @@ public:
         int prev_swap = 1, prev_no_swap = 0;
         if (n > 1) {
             for (i = 1; i < n; i++) {
-                swap = INT_MAX;
-                no_swap = INT_MAX;
-                if (A[i] > A[i-1] && B[i] > B[i-1]) {
+                if (A[i] <= A[i-1] || B[i] <= B[i-1]) {
+                    swap = prev_no_swap+1;
+                    no_swap = prev_swap;
+                }
+                else if (A[i] > B[i-1] && B[i] > A[i-1]) {
+                    swap = min(prev_swap,prev_no_swap)+1;
+                    no_swap = min(prev_swap,prev_no_swap);
+                }
+                else {
                     swap = prev_swap+1;
                     no_swap = prev_no_swap;
-                }
-                if (A[i] > B[i-1] && B[i] > A[i-1]) {
-                    no_swap = min(no_swap,prev_swap);
-                    swap = min(prev_no_swap,swap)+1;
                 }
                 prev_swap = swap;
                 prev_no_swap = no_swap;
