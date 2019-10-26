a778c42e2c1047fb6e754e357d1484ebe8e2749c
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2019-10-26 17:27:01 -0400

    Longest Repeating Character Replacement

---
+++ longestrepeatingcharreplacement.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int most_frequent(vector<int> &count) {
+        int ans = INT_MIN;
+        for (auto &x : count)
+            ans = max(ans,x);
+        return ans;
+    }
+    
+    int characterReplacement(string s, int k) {
+        int l = 0, r = 0, n = s.length(), ans = 0;
+        int most = INT_MIN;
+        vector<int> freqs(26,0);
+        while (r < n) {
+            freqs[s[r]-'A']++;
+            if (freqs[s[r++]-'A'] > most) 
+                most = freqs[s[r-1]-'A'];
+            while (l < r && r-l-most > k) {
+                freqs[s[l++]-'A']--;
+                most = most_frequent(freqs);
+            }
+            ans = max(ans,r-l);
+        }
+        return ans;
+    }
+};
