c0d74fdfb715a7955c0de137926c67f2464a16cd
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2019-10-26 11:15:20 -0400

    Longest Substring with At Most Two Distinct Characters

---
+++ longestsubstrwithatmost2distinctchars.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int lengthOfLongestSubstringTwoDistinct(string s) {
+        int l = 0, r = 0, n = s.length();
+        int unique = 0, longest = 0;
+        unordered_map<char,int> freqs;
+        while (r < n) {
+            freqs[s[r]]++;
+            if (freqs[s[r++]] == 1)
+                unique++;
+            if (unique < 3)
+                longest = max(longest,r-l);
+            else {
+                while (unique > 2) {
+                    freqs[s[l]]--;
+                    if (!freqs[s[l++]])
+                        unique--;
+                }
+                longest = max(longest,r-l);
+            }
+        }
+        return longest;
+    }
+};
