36711afe64735411fa7f938e25ae639f68570ac9
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2020-04-03 19:24:47 -0400

    Filter Restaurants by Vegan-Friendly, Price and Distance

--- filterrestaurants.cpp
+++ filterrestaurants.cpp
@@ -1,10 +1,10 @@
 class Solution {
 private:
     struct Compare {
-        bool operator()(vector<int>  &A, vector<int> &B) {
-            if (A[1] == B[1])
-                return A[0] > B[0];
-            return A[1] > B[1];
+        bool operator()(pair<int, int>  &A, pair<int, int> &B) {
+            if (A.second == B.second)
+                return A.first > B.first;
+            return A.second > B.second;
         }
     };
     
@@ -12,13 +12,16 @@ public:
     vector<int> filterRestaurants(vector<vector<int>>& restaurants, int veganFriendly, int maxPrice, int maxDistance) {
         int i, n = restaurants.size();
         vector<int> ans;
-        sort(restaurants.begin(), restaurants.end(), Compare());
+        vector<pair<int,int> > filtered;
         for (auto &x : restaurants) {
             if (veganFriendly && !x[2]) 
                 continue;
             if (x[3] <= maxPrice && x[4] <= maxDistance)
-                ans.push_back(x[0]);
+                filtered.push_back({x[0],x[1]});
         }
+        sort(filtered.begin(), filtered.end(), Compare());
+        for (auto &x : filtered) 
+            ans.push_back(x.first);
         return ans;
     }
 };
