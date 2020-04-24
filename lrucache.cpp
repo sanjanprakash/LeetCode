e7aadb50bdd3f6ea8abc7ead9a25119c154ff17f
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2020-04-24 11:36:30 -0400

    LRU Cache

--- lrucache.cpp
+++ lrucache.cpp
@@ -1,8 +1,9 @@
 class LRUCache {
 public:
-    unordered_map<int,int> X;
     int cap;
-    deque<int>recent;
+    list<pair<int, int> >recent;
+    unordered_map<int,list<pair<int, int> >::iterator > X;
+    
     LRUCache(int capacity) {
         cap = capacity;
     }
@@ -10,30 +11,37 @@ public:
     int get(int key) {
         if (X.find(key) == X.end())
             return -1;
-        else 
-            recent.erase(remove(recent.begin(), recent.end(), key), recent.end());
-        recent.push_back(key);
-        return X[key];
+        else {
+            auto p = X[key];
+            int val = p->second;
+            recent.erase(p);
+            recent.push_front({key, val});
+            X[key] = recent.begin();
+            return val;
+        }
     }
     
     void put(int key, int value) {
-        int least_recent;
-        if (X.size() == cap && X.find(key) == X.end()) {
-            least_recent = recent.front();
-            recent.pop_front();
-            X.erase(least_recent);
+        if (X.find(key) != X.end()) {
+            auto p = X[key];
+            recent.erase(p);
         }
         else {
-            recent.erase(remove(recent.begin(), recent.end(), key), recent.end());
+            if (X.size() == cap) {
+                auto least_recent = recent.rbegin();
+                X.erase(least_recent->first);
+                recent.pop_back();
+            }
         }
-        recent.push_back(key);
-        X[key] = value;
+        recent.push_front({key, value});
+        X[key] = recent.begin();
     }
 };
 
+
 /**
  * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
+ * LRUCache* obj = new LRUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
  */
