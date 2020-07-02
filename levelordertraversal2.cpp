eb68c1f40b594aea7ce241c347eb68c92d5f0937
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2020-07-02 09:25:58 -0400

    Binary Tree Level Order Traversal II

--- levelordertraversal2.cpp
+++ levelordertraversal2.cpp
@@ -4,33 +4,44 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
 public:
     vector<vector<int>> levelOrderBottom(TreeNode* root) {
-        int i, size;
+        int s;
         TreeNode *curr;
-        vector<vector<int> > ans;
-        queue<TreeNode *> Q;
+        queue<TreeNode *> q;
+        stack<vector<int>> allLevels;
+        vector<int> level;
+        vector<vector<int>> ans;
+        
         if (root) {
-            Q.push(root);
-            while (!Q.empty()) {
-                size = Q.size();
-                vector<int> uno_level;
-                for (i = 0; i < size; i++) {
-                    curr = Q.front();
-                    Q.pop();
-                    uno_level.push_back(curr->val);
+            s = 1;
+            q.push(root);
+            while (!q.empty()) {
+                while (s--) {
+                    curr = q.front();
+                    q.pop();
                     if (curr->left)
-                        Q.push(curr->left);
+                        q.push(curr->left);
                     if (curr->right)
-                        Q.push(curr->right);
+                        q.push(curr->right);
+                    level.push_back(curr->val);
                 }
-                ans.insert(ans.begin(),uno_level);
+                allLevels.push(level);
+                level.clear();
+                s = q.size();
+            }
+            while (!allLevels.empty()) {
+                ans.push_back(allLevels.top());
+                allLevels.pop();
             }
         }
+        
         return ans;
     }
 };
