805102fe3334e51fb2b44d32822323cafbb53e40
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2020-07-08 10:39:12 -0400

    3Sum

--- 3sum.cpp
+++ 3sum.cpp
@@ -1,40 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int> > ans;
-        int n = nums.size();
-        if (n == 0)
-            return ans;
+        int i, j, n = nums.size();
+        int l, r, sum;
         vector<int> temp;
-        sort(nums.begin(),nums.end());
-        int i, l, r;
-        for (i = 0; i < n - 2; i++) {
-            if ((i > 0 && nums[i] != nums[i - 1]) || i == 0) {
-                l = i + 1;
-                r = n - 1;
-                while (l < r) {
-                    if (nums[i] + nums[l] + nums[r] == 0) {
-                        temp.push_back(nums[i]);
-                        temp.push_back(nums[l]);
-                        temp.push_back(nums[r]);
-                        ans.push_back(temp);
-                        temp.clear();
-                        l++;
-                        while (nums[l] == nums[l - 1])
-                            l++;
-                        r--;
-                        while (nums[r] == nums[r + 1])
-                            r--;
-                    }
-                    else if (nums[i] + nums[l] + nums[r] < 0) {
-                        l++;
-                    }
-                    else if (nums[i] + nums[l] + nums[r] > 0) {
-                        r--;
-                    }
+        set<vector<int>> possibilities;
+        vector<vector<int> > ans;
+        sort(nums.begin(), nums.end());
+        for (i = 0; i < n-2; i++) {
+            l = i+1, r = n-1;
+            while (l < r) {
+                sum = nums[i]+nums[l]+nums[r];
+                if (sum == 0) {
+                    possibilities.insert({nums[i], nums[l], nums[r]});
+                    l++;
+                    r--;
                 }
+                else if (sum > 0)
+                    r--;
+                else
+                    l++;
             }
         }
+        for (auto &x : possibilities)
+            ans.push_back(x);
+        
         return ans;
     }
 };
