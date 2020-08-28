ca6ef06ec91f8919734879956ef5441cd4260aac
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2020-08-28 09:05:25 -0400

    Implement Rand10() Using Rand7()

---
+++ rand10usingrand7.cpp
@@ -0,0 +1,18 @@
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+
+class Solution {
+public:
+    int rand10() {
+        int r, c, idx;
+        while (true) {
+            r = rand7();
+            c = rand7();
+            idx = r + (c-1)*7;
+            if (idx <= 40)
+                break;
+        }
+        return 1 + (idx-1)%10;
+    }
+};
