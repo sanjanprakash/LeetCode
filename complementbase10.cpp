5dff43d488e284bcbfdccce4ff02d3d0124642a0
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2020-10-05 19:01:50 -0500

    Complement of Base 10 Integer

--- complementbase10.cpp
+++ complementbase10.cpp
@@ -1,19 +1,12 @@
 class Solution {
 public:
     int bitwiseComplement(int N) {
-        int mult = 1, ans = 0;
-        vector<int> bits;
+        int k = 1;
         if (!N)
             return 1;
-        while (N) {
-            bits.push_back(N%2);
-            N /= 2;
-        }
-        for (auto &x : bits) {
-            if (!x)
-                ans += mult;
-            mult *= 2;
-        }
-        return ans;
+        while (k <= N)
+            k *= 2;
+        k--;
+        return k-N;
     }
 };
