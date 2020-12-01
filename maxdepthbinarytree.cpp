24c2b53b5909c04c304b0a371b5e76e782801324
Sanjan Prakash Kumar <38526131+sanjanprakash@users.noreply.github.com>
2020-12-01 08:24:21 -0600

    Maximum Depth of Binary Tree

--- maxdepthbinarytree.cpp
+++ maxdepthbinarytree.cpp
@@ -4,26 +4,16 @@
  *     int val;
  *     TreeNode *left;
  *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class Solution {
-private:
-    int max(int a, int b) {
-        return a < b ? b : a;
-    }
-    
-    int diver(int height, TreeNode *root) {
-        if (root == NULL || (root->left == NULL && root->right == NULL))
-            return height;
-        height = max(diver(height+1,root->left),diver(height+1,root->right));
-        return height;
-    }
-    
 public:
     int maxDepth(TreeNode* root) {
-        if (root == NULL)
+        if (!root)
             return 0;
-        return diver(1,root);   
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
     }
 };
